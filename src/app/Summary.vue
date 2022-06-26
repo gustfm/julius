0f28eb4bd15c2f28346cd4e54def3cf2c0faf237
Gustavo <gustavo@solucx.com.br>
2022-06-26 20:35:57 -0300

    feat: configured project and dependencies

---
+++ src/app/Summary.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="summary-container">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Summary extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.summary-container {
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,16 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
+
+const Summary = () =>
+  import(/* webpackChunkName: "summary" */ "../app/Summary.vue");
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    name: "summary",
+    component: Summary,
   },
 ];
 
