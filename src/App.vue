917978925d26c5a43ac8f14708260e633cb29387
Gustavo <gustavo@solucx.com.br>
2022-06-29 19:25:30 -0300

    init

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,12 @@
     <router-view />
   </div>
 </template>
+
+<style lang="scss" scoped>
+#app {
+  font-family: "Montserrat", sans-serif;
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/app/Summary.vue
+++ src/app/Summary.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="summary-container">
-    
+    <div class="cards">
+      <div class="card">
+        <small class="font-normal context-title">Total incoming</small>
+        <div class="flex">
+          <small class="mr-1">R$</small>
+          <p class="font-medium text-3xl main-value">2.700,00</p>
+        </div>
+
+        <div></div>
+      </div>
+      <div class="card">a</div>
+      <div class="card">a</div>
+    </div>
   </div>
 </template>
 
@@ -13,5 +25,26 @@ export default class Summary extends Vue {}
 
 <style lang="scss" scoped>
 .summary-container {
+  .cards {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 20px;
+    padding: 20px;
+
+    .card {
+      background-color: #242424;
+      border-radius: 4px;
+      padding: 12px;
+      color: #f8f8f8;
+
+      .main-value {
+        line-height: 1;
+      }
+      .context-title {
+        color: #cecdcd;
+        font-size: 9pt;
+      }
+    }
+  }
 }
 </style>
